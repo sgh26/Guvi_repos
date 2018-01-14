4f992e5c40a634d674a0164884d525b2f50623e7
sgh26 <35269605+sgh26@users.noreply.github.com>
2018-01-14 19:36:02 +0530

    Program to check if a number is zero, positive or negative

---
+++ posORneg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void prn(int n){
+	n==0?printf("Zero"):n>0?printf("Positive"):printf("Negative");
+}
+
+	printf("Enter a number");
+	prn(n);
+	getch();
+}
+
+	
