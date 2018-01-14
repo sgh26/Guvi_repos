33c03e9de9383e718b16b05598aa1d7b9f4ce9ac
sgh26 <35269605+sgh26@users.noreply.github.com>
2018-01-14 20:22:11 +0530

    Add files via upload

--- posORneg.c
+++ posORneg.c
@@ -5,9 +5,13 @@ void prn(int n){
 	n==0?printf("Zero"):n>0?printf("Positive"):printf("Negative");
 }
 
+int main(){
+	int n;
 	printf("Enter a number");
+	scanf("%d",&n);
 	prn(n);
 	getch();
+	return 0;
 }
 
 	
